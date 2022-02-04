42580085ba52f40a6a8861a913ff2ce053f44c1b
mwruz <akbaruzmwr@gmail.com>
2022-02-04 09:22:37 +0300

    adding login popup

--- js/main.js
+++ js/main.js
@@ -21,3 +21,19 @@ $('.site-hero__carousel').slick({
     autoplaySpeed: 3000,
     arrows: false
 });
+
+
+
+// SITE HEADER LOGIN POPUP 
+
+let loginPopup = document.querySelector('.site-header__popup-wrapper');
+let loginBtn = document.querySelector('.site-header__login');
+let popupCloseIcon = document.querySelector('.popup-close');
+
+loginBtn.addEventListener('click', ()=>{
+    loginPopup.classList.add('site-header__popup-wrapper-active');
+})
+
+popupCloseIcon.addEventListener('click', ()=> [
+    loginPopup.classList.remove('site-header__popup-wrapper-active')
+])
