e9939bc91d7498fcf55ac1cb7bde4b766553a404
mwruz <akbaruzmwr@gmail.com>
2022-02-21 11:42:48 +0500

    adding esantional links

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let dotsItem = document.querySelector('.slick-dots')
+
 
 $('.site-hero__carousel').slick({
     dots: true,
@@ -21,6 +21,43 @@ $('.site-hero__carousel').slick({
     autoplaySpeed: 3000,
     arrows: false
 });
+
+
+$('.links__list').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
 
 
