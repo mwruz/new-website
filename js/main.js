8ba54e560c444ba760851ea37285605af9f97259
mwruz <akbaruzmwr@gmail.com>
2022-02-24 11:42:27 +0500

    adding fotogallery

--- js/main.js
+++ js/main.js
@@ -61,6 +61,25 @@ $('.links__list').slick({
 
 
 
+    $('.popup-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        titleSrc: function(item) {
+          return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+        }
+      }
+    });
+
+
 // SITE HEADER LOGIN POPUP 
 
 let loginPopup = document.querySelector('.site-header__popup-wrapper');
